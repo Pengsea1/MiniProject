02e83bcda902a791e5c3feb03a90a9cd7e9739be
pengsea1 <pengsea.long@gmail.com>
2019-06-15 18:54:02 +0700

    testing

---
+++ src/main/java/com/example/demo/repository/model/Category.java
@@ -0,0 +1,43 @@
+package com.example.demo.repository.model;
+
+public class Category {
+    int cateId;
+    String cateTitle;
+
+
+    public Category(int cateId, String cateTitle) {
+        this.cateId = cateId;
+        this.cateTitle = cateTitle;
+    }
+
+    public Category(String cateTitle) {
+        this.cateTitle = cateTitle;
+    }
+
+    public Category() {
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "cateId=" + cateId +
+                ", cateName='" + cateTitle + '\'' +
+                '}';
+    }
+
+    public int getCateId() {
+        return cateId;
+    }
+
+    public void setCateId(int cateId) {
+        this.cateId = cateId;
+    }
+
+    public String getCateTitle() {
+        return cateTitle;
+    }
+
+    public void setCateTitle(String cateTitle) {
+        this.cateTitle = cateTitle;
+    }
+}
